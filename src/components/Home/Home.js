056f3affb5a6c68e0ff0f06cab035420687d5b51
Lucack <lucak260@gmail.com>
2023-12-20 01:46:24 -0300

    Ajust

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,8 +21,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> Lucas Santana Santos</strong>
+                Eu sou 
+                <strong className="purple"> Lucas Santana Santos</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
