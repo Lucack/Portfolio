3f9018ee5b8af5ecc0caf207ba36eef07bcbabfa
Lucack <lucak260@gmail.com>
2024-03-08 11:19:29 -0300

    Add new projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,9 @@ import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import cris from "../../Assets/Projects/cris.png";
+import ponggame from "../../Assets/Projects/ponggame.png";
+import bouncegame from "../../Assets/Projects/bouncegame.png";
+
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
@@ -31,12 +34,40 @@ function Projects() {
               demoLink="https://lucack.itch.io/cris-adventures"
               android="https://play.google.com/store/apps/details?id=com.godot.crisadventures"
             />
+            
+    
           </Col>
+
+          <Col md={4} className="project-card">
+          <ProjectCard
+              imgPath={ponggame}
+              isBlog={false}
+              title="Pong Game"
+              description="Meu segundo jogo desenvolvido em Python utilizando Kivy, 2 jogadores. Disponível para Android! "
+              // ghLink="https://github.com/soumyajit4419/Chatify"
+              demoLink="https://lucack.itch.io/pong-game"
+              
+            />
+            </Col>
+
+            <Col md={4} className="project-card">
+          <ProjectCard
+              imgPath={bouncegame}
+              isBlog={false}
+              title="Bounce Game"
+              description="Meu primeiro jogo desenvolvido do zero em Python utilizando Tkinter. Disponível para Windows! "
+              // ghLink="https://github.com/soumyajit4419/Chatify"
+              demoLink="https://lucack.itch.io/bounce-game"
+              
+            />
+            </Col>
+          
 
           
         </Row>
       </Container>
     </Container>
+    
   );
 }
 
